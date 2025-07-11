e716eede3ca23677b02f4fff7153c9446cea4cf6
Bonjour <bonjour866@mail.ustc.edu.cn>
2025-07-11 11:27:06 +0800

    [Feat] Add support for Dr.GRPO algorithm. Provide a better format reward funtion for countdown task.

--- scripts/train_tiny_zero_a100_drgrpo.sh
+++ scripts/train_tiny_zero_a100_drgrpo.sh
@@ -1,7 +1,8 @@
-#this is training script for grpo algorithm
+# this is a training script for Dr.GRPO algorithm
 
 /home/zhangyi/miniconda3/envs/wcf-zero-py3.10/bin/python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
+    algorithm.norm_adv_by_std_in_grpo=False \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
     data.train_batch_size=128 \
@@ -14,9 +15,10 @@
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size=4 \
-    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.loss_agg_mode=seq-mean-token-sum-norm \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.grad_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
@@ -30,7 +32,7 @@
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['swanlab'] \
-    +trainer.val_before_train=False \
+    ++trainer.val_before_train=False \
     trainer.default_hdfs_dir=null \
     trainer.n_gpus_per_node=$N_GPUS \
     trainer.nnodes=1 \
